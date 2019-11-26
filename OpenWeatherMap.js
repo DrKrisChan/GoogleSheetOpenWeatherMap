609eba6e07e6935a6e3c6424df52e9fd362af77f
DrKrisChan <58226561+DrKrisChan@users.noreply.github.com>
2019-11-26 16:24:21 +0000

    Add files via upload

---
+++ OpenWeatherMap.js
@@ -0,0 +1,30 @@
+// Original Code written by Amit Agarwal www.ctrlq.org
+// Amended by Dr Kristofer Chan (kristofer.chan@kcl.ac.uk) to acquire weather data from OpenWeatherMap
+
+// Step 1: Setup an OpenWeatherMap account and find the API key
+
+// Step 2: EDIT THE LATITUDE (lat=), LONGITUDE (lon=) AND API KEY (APPID) BELOW
+
+var response = UrlFetchApp.fetch("https://api.openweathermap.org/data/2.5/weather?lat=51.4816&lon=-3.1791&appid=b6907d289e10d714a6e88b30761fae22"); 
+Logger.log(response)
+
+function writeJSONtoSheet(json) {
+
+  var sheet = SpreadsheetApp.getActiveSheet();
+
+
+  var row = [];
+  row.push(response);
+ 
+
+  sheet.appendRow(row);
+
+}
+
+// Step 3: After the above has been edited:
+// a) We recommend renaming 1) the function (change Tawarano to your location), 2) "code.gs" (left toolbar), and 3) your project title (top toolbar)
+// b) SAVE (CTRL+S)
+
+// Step 4: Under "Publish" on the top bar, click "Deploy as web app". Click Update and allow permissions via Google
+
+// Step 5: navigate to script.google.com. For the specific web app, click ... then Triggers. Click "create a new trigger", "Event source" -> "Time Driven" and set an appropriate interval (10-15 minutes is generally good).
